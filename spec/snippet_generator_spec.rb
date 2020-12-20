3eb3956e50ea879fc25d678837def0c874443d8d
AJ Markow <alexanderjmarkow@gmail.com>
2020-12-19 16:39:36 -0800

    Add test for single snippet export. Pass test for single snipet export.

--- spec/snippet_generator_spec.rb
+++ spec/snippet_generator_spec.rb
@@ -8,4 +8,11 @@ describe ('initialize_espanso_yml') do
   initialize_espanso_yml("./test.yml")
   expect(File.read("test.yml")).to(eq("matches:"+NEW_LINE)) 
    end 
+    end
+
+describe ('single_snippet_export') do 
+  it('takes arguments provided and inserts them into :trigger and replace respectively, in quotes.  ') do 
+    single_snippet_export("test.yml","hello","whats up")
+    expect(File.read("test.yml")).to(eq("matches:"+NEW_LINE+"  - trigger: "+"\":hello\"\n    replace: \"whats up\""+NEW_LINE)) 
     end 
+      end 
